441cc1f993bb758b048921ad7783150911e6bc3b
Farhatjan <faralost@gmail.com>
2022-01-06 21:38:48 +0600

    created model Product, added it to admin panel and made migrations

--- productsapp/admin.py
+++ productsapp/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+from productsapp.models import Product
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'category', 'price', 'balance']
+    list_filter = ['category']
+    search_fields = ['name']
+    fields = ['name', 'description', 'category', 'balance', 'price']
+
+
+admin.site.register(Product, ProductAdmin)
--- productsapp/models.py
+++ productsapp/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    CATEGORY_CHOICES = [('other', 'Разное'), ('drinks', 'Напитки'), ('food', 'Еда'),
+                        ('books', 'Книги'), ('clothes', 'Одежда')]
+
+    name = models.CharField(max_length=100, verbose_name='Название товара')
+    description = models.TextField(max_length=2000, null=True, blank=True, default=None,
+                                   verbose_name='Описание товара')
+    category = models.CharField(max_length=20, choices=CATEGORY_CHOICES, default='other',
+                                verbose_name='Категория товара')
+    balance = models.PositiveIntegerField(default=0, verbose_name='Остаток товара')
+    price = models.DecimalField(max_digits=7, decimal_places=2, verbose_name='Цена товара')
+
+    def __str__(self):
+        return f"{self.pk}. {self.name}: {self.price}"
+
+    class Meta:
+        db_table = 'products'
+        verbose_name = 'Товар'
+        verbose_name_plural = 'Товары'
