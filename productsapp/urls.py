8d090948aa66745f7c8dee884a60ba047907ae37
Farhatjan <faralost@gmail.com>
2022-01-07 15:40:16 +0600

    added detailed view of one product

--- productsapp/urls.py
+++ productsapp/urls.py
@@ -1,11 +1,10 @@
 from django.urls import path
 
-from productsapp.views import index
+from productsapp.views import index, product_view
 
 urlpatterns = [
     path('', index, name='index'),
-    # path('tasks/', tasks_list_view, name='tasks_list_view'),
-    # path('task/<int:pk>/', task_view, name='task_view'),
+    path('product/<int:pk>/', product_view, name='product_view'),
     # path('tasks/add/', task_create, name='task_add'),
     # path('task/<int:pk>/delete/', task_delete, name='task_delete'),
     # path('task/<int:pk>/update/', task_update_view, name='task_update'),
--- productsapp/views.py
+++ productsapp/views.py
@@ -1,5 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+
+from productsapp.models import Product
 
 
 def index(request):
-    return render(request, 'index.html')
+    if request.method == 'GET':
+        products = Product.objects.all()
+        return render(request, 'index.html', {'products': products})
+    else:
+        pass
+
+
+def product_view(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    return render(request, 'product_view.html', {'product': product})
+
