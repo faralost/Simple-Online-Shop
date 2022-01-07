c8ba0f9186dbc0190c133fded99194a70e404020
Farhatjan <faralost@gmail.com>
2022-01-07 16:45:40 +0600

    added updating a product

--- productsapp/urls.py
+++ productsapp/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
 
-from productsapp.views import index, product_view, product_add
+from productsapp.views import index, product_view, product_add, product_update
 
 urlpatterns = [
     path('', index, name='index'),
     path('product/<int:pk>/', product_view, name='product_view'),
     path('product/add/', product_add, name='product_add'),
     # path('task/<int:pk>/delete/', task_delete, name='task_delete'),
-    # path('task/<int:pk>/update/', task_update_view, name='task_update'),
+    path('product/<int:pk>/update/', product_update, name='product_update'),
 ]
--- productsapp/views.py
+++ productsapp/views.py
@@ -27,3 +27,27 @@ def product_add(request):
             new_product = form.save()
             return redirect('product_view', pk=new_product.pk)
         return render(request, 'product_add.html', {'form': form})
+
+
+def product_update(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    if request.method == 'GET':
+        form = ProductForm(initial={
+            'name': product.name,
+            'description': product.description,
+            'category': product.category,
+            'balance': product.balance,
+            'price': product.price
+        })
+        return render(request, 'product_update.html', {'product': product, 'form': form})
+    elif request.method == 'POST':
+        form = ProductForm(data=request.POST)
+        if form.is_valid():
+            product.name = form.cleaned_data.get('name')
+            product.description = form.cleaned_data.get('description')
+            product.category = form.cleaned_data.get('category')
+            product.balance = form.cleaned_data.get('balance')
+            product.price = form.cleaned_data.get('price')
+            product.save()
+            return redirect('product_view', pk=product.pk)
+        return render(request, 'product_update.html', {'product': product, 'form': form})
