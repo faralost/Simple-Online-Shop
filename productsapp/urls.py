f20d006b29e7f58bd9cc645a50ab557986bfb47f
Farhatjan <faralost@gmail.com>
2022-02-03 21:20:45 +0600

    refactored products index view using custom SearchListView inherited from ListView

--- productsapp/urls.py
+++ productsapp/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from productsapp.views import index, product_view, product_add, product_update, product_delete, products_category
+from productsapp.views import product_view, product_add, product_update, product_delete, products_category, \
+    ProductsIndexView
 
 urlpatterns = [
-    path('', index, name='index'),
+    path('', ProductsIndexView.as_view(), name='index'),
     path('product/<int:pk>/', product_view, name='product_view'),
     path('product/add/', product_add, name='product_add'),
     path('product/<int:pk>/delete/', product_delete, name='product_delete'),
--- productsapp/views.py
+++ productsapp/views.py
@@ -1,16 +1,26 @@
 from django.shortcuts import render, get_object_or_404, redirect
 
+from productsapp.base import SearchListView
 from productsapp.forms import ProductForm
 from productsapp.models import Product
 
 
-def index(request):
-    search = request.GET.get('search')
-    products = Product.objects.filter(balance__gt=0).order_by('category', 'name')
-    if search:
-        products = Product.objects.filter(name__icontains=search).order_by('category', 'name')
-        return render(request, 'index.html', {'products': products, 'categories': Product.CATEGORY_CHOICES})
-    return render(request, 'index.html', {'products': products, 'categories': Product.CATEGORY_CHOICES})
+class ProductsIndexView(SearchListView):
+    model = Product
+    template_name = 'index.html'
+    context_object_name = 'products'
+    paginate_by = 10
+    ordering = ['category', 'name']
+    search_fields = ['name__icontains']
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        return queryset.filter(balance__gt=0)
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['categories'] = Product.CATEGORY_CHOICES
+        return context
 
 
 def product_view(request, pk):
