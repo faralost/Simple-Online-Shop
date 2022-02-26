f43a96c5e4d1dea53f139d961980ec68213fb32c
Farhatjan <faralost@gmail.com>
2022-02-26 15:30:45 +0600

    added foreign key user field to Order model

--- productsapp/models.py
+++ productsapp/models.py
@@ -1,5 +1,8 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 from django.urls import reverse
+
+User = get_user_model()
 
 
 class Product(models.Model):
@@ -55,6 +58,8 @@ class Order(models.Model):
     created_at = models.DateTimeField(auto_now_add=True, verbose_name="Дата создания")
     products = models.ManyToManyField("productsapp.Product", related_name="orders", through="productsapp.OrderProduct",
                                       through_fields=("order", "product"))
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='users', verbose_name='Пользователи',
+                             null=True, blank=True)
 
     def __str__(self):
         return f"{self.customer_name}"
