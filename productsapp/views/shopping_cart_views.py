87512279eb957cffbafeba440df5a391857e6161
Farhatjan <faralost@gmail.com>
2022-02-28 16:02:26 +0600

    refactored ShoppingCartDelete so that now it deletes products cart by one

--- productsapp/views/shopping_cart_views.py
+++ productsapp/views/shopping_cart_views.py
@@ -60,68 +60,27 @@ class ShoppingCartDetailView(CreateView):
         for item in self.cart.values():
             item['price'] = Decimal(item['price'])
             item['total_price'] = item['price'] * item['qty']
-        print(self.cart)
         return self.cart
 
     def get_total(self):
         return sum(Decimal(item['price']) * item['qty'] for item in self.cart.values())
 
-    # def get_total_price(self):
-    #     return sum(Decimal(item['price']) * item['quantity'] for item in self.cart.values())
 
-    # form_class = OrderForm
-    # model = Order
-    # template_name = 'shopping_cart/detail_view.html'
-    #
-    # def get_context_data(self, *, object_list=None, **kwargs):
-    #     context = super(ShoppingCartDetailView, self).get_context_data(**kwargs)
-    #     context['shopping_cart'] = ShoppingCart.objects.all()
-    #     context['title'] = 'Корзина'
-    #     context['categories'] = Product.CATEGORY_CHOICES
-    #     context['total'] = self.get_total()
-    #     return context
-    #
-    # def form_valid(self, form):
-    #     self.object = form.save()
-    #     self.object.user = self.request.user
-    #     self.get_order()
-    #     self.delete_shopping_cart()
-    #     return super().form_valid(form)
-    #
-    # def delete_shopping_cart(self):
-    #     ShoppingCart.objects.all().delete()
-    #
-    # def get_order(self):
-    #     for item in ShoppingCart.objects.all():
-    #         order_list = OrderProduct.objects.create(order_id=self.object.pk, product_id=item.product_id,
-    #                                                  quantity=item.quantity)
-    #         Product.objects.filter(id=item.product_id).update(balance=F('balance')-item.quantity)
-    #     return order_list
-    #
-    # def get_total(self):
-    #     total = ShoppingCart.objects.annotate(mult=F('quantity') * F('product__price')).aggregate(sum=Sum('mult'))
-    #     return total
-    #
-    # def get_success_url(self):
-    #     return reverse('productsapp:shopping_cart_view')
-
-
-class ShoppingCartDeleteView(DeleteView):
-    pass
-    # model = ShoppingCart
-    #
-    # def get(self, request, *args, **kwargs):
-    #     return self.delete(request, *args, **kwargs)
-    #
-    # def get_success_url(self):
-    #     return reverse('productsapp:shopping_cart_view')
-    #
-    # def delete(self, *args, **kwargs):
-    #     quantity = ShoppingCart.objects.filter(pk=self.kwargs['pk']).update(quantity=F('quantity') - 1)
-    #     self.object = self.get_object()
-    #     if self.object.quantity >= quantity:
-    #         return redirect('productsapp:shopping_cart_view')
-    #     return super(ShoppingCartDeleteView, self).delete(*args, **kwargs)
+class ShoppingCartDeleteView(View):
+    def get(self, request, *args, **kwargs):
+        product = get_object_or_404(Product, pk=self.kwargs['pk'])
+        cart = self.request.session['cart']
+        if cart[str(product.pk)]['qty'] <= 1:
+            del cart[str(product.pk)]
+            self.request.session['cart'] = cart
+            product.balance += 1
+            product.save()
+        else:
+            cart[str(product.pk)]['qty'] -= 1
+            self.request.session['cart'] = cart
+            product.balance += 1
+            product.save()
+        return redirect('productsapp:shopping_cart_view')
 
 
 class UserOrdersView(LoginRequiredMixin, ListView):
