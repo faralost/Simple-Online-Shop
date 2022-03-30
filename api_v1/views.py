35fb812b68bbaf27e4b314bbd4110bc6859bae4d
Farhatjan <faralost@gmail.com>
2022-03-30 21:34:03 +0600

    gave permissions to view products and make orders by anyone. add, update, delete products and view orders only to is_staff

--- api_v1/views.py
+++ api_v1/views.py
@@ -1,5 +1,6 @@
-from rest_framework import viewsets, generics, mixins
+from rest_framework import viewsets, mixins
 from rest_framework.viewsets import GenericViewSet
+from rest_framework.permissions import SAFE_METHODS, IsAdminUser, AllowAny
 
 from api_v1.serializers import ProductSerializer, OrderSerializer, UserSerializer
 from productsapp.models import Product, Order, User
@@ -9,6 +10,11 @@ class ProductViewSet(viewsets.ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
 
+    def get_permissions(self):
+        if self.request.method not in SAFE_METHODS:
+            self.permission_classes = [IsAdminUser]
+        return super().get_permissions()
+
 
 class OrderViewSet(mixins.ListModelMixin, mixins.RetrieveModelMixin, mixins.CreateModelMixin, GenericViewSet):
     queryset = Order.objects.all()
@@ -16,6 +22,17 @@ class OrderViewSet(mixins.ListModelMixin, mixins.RetrieveModelMixin, mixins.Crea
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
+
+    permission_classes_by_action = {
+        "create": [AllowAny],
+        "list": [IsAdminUser],
+    }
+
+    def get_permissions(self):
+        try:
+            return [permission() for permission in self.permission_classes_by_action[self.action]]
+        except KeyError:
+            return [permission() for permission in self.permission_classes]
 
 
 class UserViewSet(viewsets.ReadOnlyModelViewSet):
